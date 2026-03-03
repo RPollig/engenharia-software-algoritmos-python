eadc4de25a73e55ba5085022d97386fe5d220a4c
RPollig <renanpolligferreira@gmail.com>
2026-03-03 18:16:40 -0300

    Exercício 05 - conversão de metros para centímetros

---
+++ listas/Lista01/Lista01exercicio05.py
@@ -0,0 +1,8 @@
+# 5. Converta metros para centímetros.
+n = int(input("Quantos metros você quer trasformar em centimetros: "))
+centimetros = n * 100
+print(n, "metros corresponde a", centimetros, "centímetros")
+
+n = float(input("Quantos metros você quer trasformar em centimetros: "))
+centimetros = n * 100
+print(n, "metros corresponde a", centimetros, "centímetros")
